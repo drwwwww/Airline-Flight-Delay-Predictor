203c054a7f4c878efca08b82430729aad0ca668a
Andrew <ledanreba@gmail.com>
2025-10-28 11:22:10 -0400

    Added sample data file as my file was too large

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 
 import json
 from pathlib import Path
+import os
 
 import joblib
 import pandas as pd
@@ -9,4 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import roc_auc_score
+
+import pandas as pd
+
+
 
