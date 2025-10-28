fb0512d50338bbdf507f27badaebd1b6a400f958
Andrew <ledanreba@gmail.com>
2025-10-28 10:56:38 -0400

    Initial Commit: Full Project Overhaul

---
+++ train.py
@@ -0,0 +1,12 @@
+
+import json
+from pathlib import Path
+
+import joblib
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import roc_auc_score
+
